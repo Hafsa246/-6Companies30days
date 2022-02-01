e4642580aabcf34d8fa0a1e4ef315c307eebb90b
Hafsa246 <81948816+Hafsa246@users.noreply.github.com>
2022-02-01 20:06:22 +0530

    Add files via upload

---
+++ Milestone 6/Code 13.cpp
@@ -0,0 +1,19 @@
+// Find the Kth Largest Integer in the Array
+
+class compare
+{
+  public:
+    bool operator()(string& a ,string& b)
+    {
+        if(a.length() == b.length())
+            return (a > b);
+        return (a.length() > b.length());
+    }
+};
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& A, int k) {
+        sort(A.begin(),A.end(),compare());
+        return A[k - 1];
+    }
+};
